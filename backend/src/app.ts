f5b8fd2062dbd7545ae009e201f07e1fcd867f4c
Reuel Mendoza <reuelmendoza29@gmail.com>
2026-08-10 23:16:38 +0800

    fix live

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,12 +17,7 @@ const app = express();
 // MIDDLEWARE
 // =====================================================
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(
   helmet({
